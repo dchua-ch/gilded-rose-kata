425afc8e8a1686363b034cbee968c554f868a1ad
Daniel <danielchua93@gmail.com>
2023-01-14 16:27:06 +0800

    refactored unit tests to make use of a parameterized test method

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,55 +19,58 @@ class GildedRoseTest {
      */
     private static final String FOO = "foo";
 
+
+    void baseTest( 
+                String itemName,
+                int initialSellIn, 
+                int finalSellIn,
+                int initialQuality,  
+                int finalQuality) {
+        
+        Item[] items = new Item[] { new Item(itemName, initialSellIn, initialQuality) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+
+        assertEquals(itemName, app.items[0].name);
+        assertEquals(finalSellIn, app.items[0].sellIn);
+        assertEquals(finalQuality,app.items[0].quality);
+    }
+
     @Test
     void baseUpdateTest() {
-        // For items whose sell by date hasn't passed
-
         final int initialSellIn = 1;
         final int initialQuality = 20;
 
-        //Arrange
-        Item[] items = new Item[] { new Item(FOO, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-
-        //Act
-        app.updateQuality();
-
-        //Assert
-        assertEquals(FOO, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality - 1,app.items[0].quality);
+        baseTest(FOO,
+                 initialSellIn,
+                 initialSellIn-1,
+                 initialQuality,
+                 initialQuality-1);
     }
 
 
     @Test
     void passedSellByDateTest() {
-
         final int initialSellIn = 0;
         final int initialQuality = 20;
 
-        Item[] items = new Item[] { new Item(FOO, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(FOO, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality - 2,app.items[0].quality);
+        baseTest(FOO,
+                 initialSellIn,
+                 initialSellIn-1,
+                 initialQuality,
+                 initialQuality-2);
     }
 
     @Test
     void nonNegativeQualityTest() {
-
         final int initialSellIn = 1;
         final int initialQuality = 0;
-
-        Item[] items = new Item[] { new Item(FOO, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(FOO, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality,app.items[0].quality);
+        baseTest(FOO,
+                 initialSellIn,
+                 initialSellIn-1,
+                 initialQuality,
+                 initialQuality);
     }
 
     @Test
@@ -75,13 +78,11 @@ class GildedRoseTest {
         final int initialSellIn = 1;
         final int initialQuality = 10;
 
-        Item[] items = new Item[] { new Item(AGED_BRIE, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(AGED_BRIE, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality + 1,app.items[0].quality);
+        baseTest(AGED_BRIE,
+                 initialSellIn,
+                 initialSellIn-1,
+                 initialQuality,
+                 initialQuality+1);
     }
 
     
@@ -90,13 +91,11 @@ class GildedRoseTest {
         final int initialSellIn = 0;
         final int initialQuality = 10;
 
-        Item[] items = new Item[] { new Item(AGED_BRIE, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(AGED_BRIE, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality + 2,app.items[0].quality);
+        baseTest(AGED_BRIE,
+                 initialSellIn,
+                 initialSellIn-1,
+                 initialQuality,
+                 initialQuality+2);
     }
 
     @Test
@@ -104,13 +103,11 @@ class GildedRoseTest {
         final int initialSellIn = 1;
         final int initialQuality = 50;
 
-        Item[] items = new Item[] { new Item(AGED_BRIE, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(AGED_BRIE, app.items[0].name);
-        assertEquals(initialSellIn - 1, app.items[0].sellIn);
-        assertEquals(initialQuality,app.items[0].quality);
+        baseTest(AGED_BRIE,
+                initialSellIn,
+                initialSellIn-1,
+                initialQuality,
+                initialQuality);
     }
 
     @Test
@@ -118,14 +115,12 @@ class GildedRoseTest {
         final int initialSellIn = 1;
         final int initialQuality = 80;
 
-        // The existing code doesn't seem to ensure that SULFURAS will always have a quality of 80
-        Item[] items = new Item[] { new Item(SULFURAS, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(SULFURAS, app.items[0].name);
-        assertEquals(initialSellIn, app.items[0].sellIn);
-        assertEquals(80,app.items[0].quality);
+         // The existing code doesn't seem to ensure that SULFURAS will always have a quality of 80
+        baseTest(SULFURAS,
+                initialSellIn,
+                initialSellIn,
+                initialQuality,
+                initialQuality);
     }
 
     @Test
@@ -133,13 +128,11 @@ class GildedRoseTest {
         final int initialSellIn = 0;
         final int initialQuality = 80;
 
-        Item[] items = new Item[] { new Item(SULFURAS, initialSellIn, initialQuality) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(SULFURAS, app.items[0].name);
-        assertEquals(initialSellIn, app.items[0].sellIn);
-        assertEquals(80,app.items[0].quality);
+        baseTest(SULFURAS,
+                initialSellIn,
+                initialSellIn,
+                initialQuality,
+                initialQuality);
     }
 
 }
