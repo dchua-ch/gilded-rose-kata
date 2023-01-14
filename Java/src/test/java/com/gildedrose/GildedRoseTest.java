9ddb1524fc90f224541d70c41bb5a18cff0ce324
Daniel <danielchua93@gmail.com>
2023-01-14 15:56:42 +0800

    added tests for base update, passed by sell date and non negative quality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void baseUpdateTest() {
+        // For items whose sell by date hasn't passed
+
+        final int initialSellIn = 1;
+        final int initialQuality = 20;
+
+        //Arrange
+        Item[] items = new Item[] { new Item("foo", initialSellIn, initialQuality) };
+        GildedRose app = new GildedRose(items);
+
+        //Act
+        app.updateQuality();
+
+        //Assert
+        assertEquals("foo", app.items[0].name);
+        assertEquals(initialSellIn - 1, app.items[0].sellIn);
+        assertEquals(initialQuality - 1,app.items[0].quality);
+    }
+
+
+    @Test
+    void passedSellByDateTest() {
+
+        final int initialSellIn = 0;
+        final int initialQuality = 20;
+
+        Item[] items = new Item[] { new Item("foo", initialSellIn, initialQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+
         assertEquals("foo", app.items[0].name);
+        assertEquals(initialSellIn - 1, app.items[0].sellIn);
+        assertEquals(initialQuality - 2,app.items[0].quality);
+    }
+
+    @Test
+    void nonNegativeQualityTest() {
+
+        final int initialSellIn = 1;
+        final int initialQuality = 0;
+
+        Item[] items = new Item[] { new Item("foo", initialSellIn, initialQuality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+        assertEquals(initialSellIn - 1, app.items[0].sellIn);
+        assertEquals(initialQuality,app.items[0].quality);
+
     }
 
 }
