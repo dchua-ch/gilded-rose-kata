67338ade1dfe10cc39de8597d08277d296544012
Daniel <danielchua93@gmail.com>
2023-01-24 00:58:39 +0800

    refactored else if block dealing with updating quality of backstage pass

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,22 +22,26 @@ class GildedRose {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;
                 }
-            } else if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+            } 
+            else if (items[i].name.equals(BACKSTAGE_PASSES) && items[i].quality < 50) {
+                if (items[i].sellIn < 11 && items[i].sellIn >= 6) {
+                    items[i].quality += 2;   
                 }
+
+                else if (items[i].sellIn < 6 && items[i].sellIn >= 0) {
+                    items[i].quality += 3;
+                }
+
+                else {
+                    items[i].quality = items[i].quality + 1;
+                }
+
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+            }
+            else if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
             }
             
 
